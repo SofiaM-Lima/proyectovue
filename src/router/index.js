e146b81ac80d4824d8f49401f0ec58947c807670
SofiaM-Lima <ML17002@ues.edu.sv>
2021-11-25 22:37:00 -0600

    estadisticas

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/estadisticas',
+    name: 'Estadisticas',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Estadisticas.vue')
   }
   
 ]
---
+++ src/views/Estadisticas.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col md="6">
+        <v-card
+          class="mx-auto text-center"
+          color="blue-grey darken-1"
+          dark
+          max-width="600"
+        >
+          <v-card-text>
+            <v-sheet color="rgba(0, 0, 0, .12)">
+              <v-sparkline
+                :value="value"
+                color="black"
+                height="100"
+                padding="24"
+                stroke-linecap="round"
+                smooth
+              >
+                <template v-slot:label="item"> ${{ item.value }} </template>
+              </v-sparkline>
+            </v-sheet>
+          </v-card-text>
+
+          <v-card-text>
+            <div class="text-h4 font-weight-thin">Ventas mes de octubre</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col md="6">
+        <v-card
+          class="mx-auto text-center"
+          color="lime darken-2"
+          dark
+          max-width="600"
+        >
+          <v-card-text>
+            <v-sheet color="rgba(0, 0, 0, .12)">
+              <v-sparkline
+                :value="valor"
+                color="rgba(255, 255, 255, .7)"
+                height="100"
+                padding="24"
+                stroke-linecap="round"
+                smooth
+              >
+                <template v-slot:label="item"> ${{ item.valor }} </template>
+              </v-sparkline>
+            </v-sheet>
+          </v-card-text>
+
+          <v-card-text>
+            <div class="text-h4 font-weight-thin">Ventas al final del año</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    value: [423, 446, 675, 510, 590, 610, 760],
+    valor: [523, 123, 990, 321, 754, 222, 875],
+  }),
+};
+</script>
